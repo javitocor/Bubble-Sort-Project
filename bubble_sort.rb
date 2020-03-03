4299a9034d4f8a48370604eb7b065b659e1169bb
javitocor <56809963+javitocor@users.noreply.github.com>
2020-03-03 10:01:49 +0100

    Added bubble sort by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,3 +13,33 @@ def bubble_sort(array)
     end
     return array
 end
+
+#Bubble_sort_by
+def bubble_sort_by(array)
+    array_length = array.length
+    array_length.times do |n|
+        (array_length - n - 1).times do |m|             
+            yield_var = yield(array[m], array[m + 1])           
+            if yield_var > 0
+                if array[m] > array[m + 1]
+                    first_num = array[m]
+                    second_num = array[m + 1]
+                    array[m] = second_num
+                    array[m + 1] = first_num
+                end
+            elsif yield_var < 0
+                if array[m] < array[m + 1]
+                    first_num = array[m]
+                    second_num = array[m + 1]
+                    array[m] = second_num
+                    array[m + 1] = first_num
+                end
+            end
+        end
+    end
+    return array
+end
+
+puts bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
